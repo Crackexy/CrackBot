a7df5f81795d879452ffe44aa2ef37910814a251
Crackexy <67500032+Crackexy@users.noreply.github.com>
2020-08-27 11:27:39 +0530

    Some Fixes...

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,12 +8,12 @@ from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "CrackBot"
 PM_IMG = "https://telegra.ph/file/3feaa0629756e0e45a2b7.jpg"
 pm_caption = "**ᴄʀᴀᴄᴋʙᴏᴛ ᴏғғɪᴄɪᴀʟ**\n\n"
-pm_caption += "**🙃 Yus, Am Alive And Working Fine Bish Don't Be Rude Go And Fuq Off...**\n\n"
+pm_caption += "**🙃 Yus Master, Am Alive And Working Fine 😎**\n\n"
 pm_caption += "✘ About My System ✘\n\n"
 pm_caption += "➾Telethon Version : **6.0.9**\n➾Python : 3.7.4\n"
 pm_caption += "➾DataBase : **Functioning**\n"
 pm_caption += "➾Bot Creator : [Crackexy](https://t.me/Crackexy).\n"
-pm_caption += "➾Crackbot Version : 1.00\n\n"
+pm_caption += "➾Crackbot Version : 1.50\n\n"
 pm_caption += f"**➾My Master** : {DEFAULTUSER}\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
