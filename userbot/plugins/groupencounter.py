baa5d17b6b98a6f5fb6550e1e9b395dd47b699d2
Crackexy <67500032+Crackexy@users.noreply.github.com>
2020-09-01 11:53:58 +0530

    Gban Fixed...

--- userbot/plugins/groupencounter.py
+++ userbot/plugins/groupencounter.py
@@ -129,7 +129,7 @@ async def gspider(rk):
             return await rkp.edit(f"**Error! User Probably Already Gbanned.**")
    except:
     	pass
-   return await rkp.edit(f"**USER GBANNED❗❗❗**\n**Person's Name** : {user.first_name}\n**Victim's Id** :{user.id}\n**GBanned By** : @{me.username}\n**Affected Chats** : {a} \n**User Was Blocked and added to CrackBot Gban List**") 
+   return await rkp.edit(f"**USER GBANNED❗❗❗**\n**Person's Name** : {user.first_name}\n**Victim's Id** :{user.id}\n**GBanned By** : @{me.username}\n**Affected Chats** : {a} \n**User Was Blocked and added to CrackBot Gban List.**") 
 
 @borg.on(admin_cmd(pattern="ungban(?: |$)(.*)"))
 async def gspider(rk):
